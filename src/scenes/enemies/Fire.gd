3a4594ebea70776c78a281d4b3f5003ce66ea902
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-11 09:49:33 -0400

    Added tileset, fire, and cloud collision detection

--- src/scenes/enemies/Fire.gd
+++ src/scenes/enemies/Fire.gd
@@ -11,7 +11,10 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
 
-func _on_Cloud_area_entered(area: Area2D) -> void:
-	if area.is_in_group("enemy"):
-		area.queue_free()
+
+func _on_Fire_area_entered(area: Area2D) -> void:
+	print("fire")
