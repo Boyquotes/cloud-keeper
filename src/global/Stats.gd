993477df0b5556cdd4721170949718b5be7e148f
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-11 21:06:38 -0400

    Created shrine scene and health bar

--- src/global/Stats.gd
+++ src/global/Stats.gd
@@ -10,3 +10,5 @@ var cloud_energy_max: float = 100.0
 var wind_energy_max: float = 100.0
 var cloud_recharge_rate: float = 5.0
 var wind_recharge_rate: float = 5.0
+
+var shrine_health: float = 100.0
