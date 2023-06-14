80a8dad2b95d11234661ae650e530750e0e8313a
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-13 23:03:14 -0400

    Disable start menu buttons when game starts

--- src/ui/start/StartMenu.gd
+++ src/ui/start/StartMenu.gd
@@ -10,6 +10,9 @@ func _ready() -> void:
 	pass
 
 func _on_PlayButton_pressed() -> void:
+	play_button.disabled = true
+	settings_button.disabled = true
+	exit_button.disabled = true
 	emit_signal("play_button_pressed")
 
 func _on_SettingsButton_pressed() -> void:
