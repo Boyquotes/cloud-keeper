35f8532416d40b62e9c6a9ef98e94728a1612da1
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-15 20:30:21 -0400

    Updated wind animation

--- src/scenes/player/WindArea.gd
+++ src/scenes/player/WindArea.gd
@@ -1,8 +1,11 @@
 extends Area2D
 
+onready var animation_player: AnimationPlayer = $"%AnimationPlayer"
+
 export(float) var wind_speed = 100.0
 
 func summon_wind(direction: Vector2):
 	var clouds = get_overlapping_areas()
 	for cloud in clouds:
 		cloud.move(direction, wind_speed)
+	animation_player.play("wind_blast")
