367b1ff91f8b8d14329b9f70cff6086fe5ebd455
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-15 20:45:31 -0400

    Added cloud faces

--- src/global/Constants.gd
+++ src/global/Constants.gd
@@ -2,3 +2,6 @@ extends Node
 
 var WIND_POTION = 1
 var CLOUD_POTION = 2
+
+var CLOUD_ANGRY_FACE: AtlasTexture = preload("res://scenes/cloud/cloud_angry_face.tres")
+var CLOUD_HAPPY_FACE: AtlasTexture = preload("res://scenes/cloud/cloud_happy_face.tres")
--- src/scenes/cloud/Cloud.gd
+++ src/scenes/cloud/Cloud.gd
@@ -1,17 +1,26 @@
 extends Area2D
 
 onready var animation_player: AnimationPlayer = $AnimationPlayer
+onready var face: Sprite = $Sprite/Face
 
 export(float) var life_time = 5.0
 var velocity: Vector2
 var friction = 70
+var face_original_pos: Vector2
 
 func _ready() -> void:
 	animation_player.play("spawn")
+	face_original_pos = face.position
 
 func _process(delta: float) -> void:
 	velocity = velocity.move_toward(Vector2.ZERO, delta * friction)
 	global_position += velocity * delta
+	if velocity == Vector2.ZERO:
+		face.texture = Constants.CLOUD_HAPPY_FACE
+		face.position = face_original_pos
+	else:
+		face.texture = Constants.CLOUD_ANGRY_FACE
+		face.position = face_original_pos + velocity.normalized() * 2
 
 func _on_Cloud_area_entered(area: Area2D) -> void:
 	if area.is_in_group("enemy"):
