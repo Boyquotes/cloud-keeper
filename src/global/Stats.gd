bc33cfd80cacae66820abb56bd5f0c56849de2cf
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-17 12:11:09 -0400

    Updated start menu buttons and removed testing states

--- src/global/Stats.gd
+++ src/global/Stats.gd
@@ -11,5 +11,5 @@ var wind_energy_max: float = 100.0
 var cloud_recharge_rate: float = 5.0
 var wind_recharge_rate: float = 5.0
 
-var shrine_health: float = 10.0
+var shrine_health: float = 100.0
 var shrine_health_max: float = 100.0
