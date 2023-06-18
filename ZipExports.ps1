8a60dc82c98de3ec5144b8c1b6286028f5fda4c3
jetpackgone <jetpackgone.dev@gmail.com>
2023-06-17 22:14:01 -0400

    Added script to zip exports

---
+++ ZipExports.ps1
@@ -0,0 +1,4 @@
+Compress-Archive -Path '.\exports\html\*' -DestinationPath '.\exports\cloud_keeper_html.zip' -Force
+Compress-Archive -Path '.\exports\windows\*' -DestinationPath '.\exports\cloud_keeper_windows.zip' -Force
+Compress-Archive -Path '.\exports\mac\*' -DestinationPath '.\exports\cloud_keeper_mac.zip' -Force
+Compress-Archive -Path '.\exports\linux\*' -DestinationPath '.\exports\cloud_keeper_linux.zip' -Force
